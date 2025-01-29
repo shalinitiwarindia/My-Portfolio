a5f00a102f55cca25f05c181abbf521a6745885a
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-01-29 22:09:06 +0530

    Update index.jsx

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -17,12 +17,7 @@ const Contact = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("http://localhost:5000/send-email", {
-        method: "POST",
-        headers: {
+ 
           "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
